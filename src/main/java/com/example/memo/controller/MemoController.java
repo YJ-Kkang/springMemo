8face3b6030e46f91edca9006c528f7f1c57c88a
YJ <kyujin995@gmail.com>
2024-12-04 08:16:05 +0900

    feat: memoController class 식별자각 1씩 증가하도록 기능 추가

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -5,6 +5,7 @@ import com.example.memo.entity.Memo;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +15,10 @@ public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
         //식별자가 1씩 증가하도록
+        //Collections.max는 이 안에 있는 것 중 최대값 뽑아냄
+        //memoList.keySet()은 memoList 안에 있는 key 값들을 다 꺼내보는  것
+        // key값을 Long으로 했음 -> 모든 Long값을 꺼내서 그 최대값을 반환해주는 것
+        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
 
         //요청받은 데이터로 Memo 객체 생성
 
