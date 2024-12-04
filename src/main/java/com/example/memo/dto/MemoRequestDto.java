6c393f07f80e3ea5bfd6bb0d49ffde35d8c11079
YJ <kyujin995@gmail.com>
2024-12-04 10:28:57 +0900

    feat: MemoRequestDto class에 기능 추가

--- src/main/java/com/example/memo/dto/MemoRequestDto.java
+++ src/main/java/com/example/memo/dto/MemoRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.memo.dto;
 
+import com.example.memo.entity.Memo;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,11 @@ public class MemoRequestDto {
     private Long id;
     private String title;
     private String contents;
+
+    //memo 객체가 RequestDto 형태로 바껴서 응답이 되어야 함
+    public MemoRequestDto(Memo memo) {
+        this.id = memo.getId();
+        this.title = memo.getTitle();
+        this.contents = memo.getContents();
+    }
 }
